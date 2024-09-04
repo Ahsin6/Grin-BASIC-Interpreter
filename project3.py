82424ac61c653efc1e23a24c73a31b96c153749e
Ahsin6 <109938515+Ahsin6@users.noreply.github.com>
2024-09-04 10:51:19 -0700

    Add files via upload

---
+++ project3.py
@@ -0,0 +1,17 @@
+# project3.py
+#
+# ICS 33 Fall 2022
+# Project 3: Why Not Smile?
+#
+# The main module that executes your Grin interpreter.
+
+
+import grin
+
+
+def main() -> None:
+    grin.run()
+
+
+if __name__ == '__main__':
+    main()
